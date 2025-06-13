8a0de4159b0eb2470aaa028b42c6720cc9d8a5b0
avneetvirdi <avneetvirdi26@gmail.com>
2025-06-13 04:27:02 -0400

    cart functionality somewhat

---
+++ src/component/CartButton.tsx
@@ -0,0 +1,27 @@
+import { useCart } from "@/context/cartContext";
+
+type propTypes={
+    name: string;
+}
+
+
+export default function CartButton({name}: propTypes) {
+
+    const { cart } = useCart();
+
+    return (
+        <div>
+            <button type="button" className="relative cursor-pointer inline-flex items-center px-5 py-2.5 text-sm font-medium text-center text-white bg-orange-950 rounded-lg hover:bg-[#9e4b34] text-md focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                <svg className="w-3.5 h-3.5 me-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 18 21">
+                    <path d="M15 12a1 1 0 0 0 .962-.726l2-7A1 1 0 0 0 17 3H3.77L3.175.745A1 1 0 0 0 2.208 0H1a1 1 0 0 0 0 2h.438l.6 2.255v.019l2 7 .746 2.986A3 3 0 1 0 9 17a2.966 2.966 0 0 0-.184-1h2.368c-.118.32-.18.659-.184 1a3 3 0 1 0 3-3H6.78l-.5-2H15Z"/>
+                </svg>
+                <span className="sr-only">Notifications</span>
+                {name}
+                <div className="absolute inline-flex items-center justify-center w-6 h-6 text-xs font-bold text-white bg-red-500 border-2 border-white rounded-full -top-2 -end-2 dark:border-gray-900">
+                    {cart.length}
+                </div>
+            </button>
+
+        </div>
+    )
+}
--- src/component/CoffeeCard.tsx
+++ src/component/CoffeeCard.tsx
@@ -1,3 +1,5 @@
+import { useCart } from "@/context/cartContext";
+
 type propTypes = {
   imgsrc: string;
   name: string;
@@ -6,11 +8,23 @@ type propTypes = {
 };
 
 export default function CoffeeCard({ imgsrc, name, roastLevel, price }: propTypes) {
+
+  const { cart, setCart } = useCart();
+  
+  const isAlreadyInCart = cart.some((item: propTypes) => item.name === name);
+
+  const addToCart = () => {
+    if (!isAlreadyInCart) {
+      const newItem = { imgsrc, name, roastLevel, price };
+      setCart([...cart, newItem]);
+    }
+  };
+
   return (
     <div className="bg-white shadow-gray-400 shadow-md rounded-lg p-4 border border-gray-200 hover:shadow-xl transition flex flex-col items-center mb-7 w-full max-w-sm">
-      <img src={imgsrc} alt="error" className="w-full h-48 object-cover rounded-md mb-4" />
+      <img src={imgsrc} alt="coffe-image" className="w-full h-48 object-contain rounded-md mb-4" />
       
-      <h2 className="text-2xl font-semibold mb-4 text-center">{name}</h2>
+      <h2 className="text-2xl font-semibold mb-4 text-center min-h-16">{name}</h2>
 
       {/* Grid layout for roast level, price, and button */}
       <div className="grid grid-cols-2 gap-4 w-full items-center">
@@ -22,9 +36,19 @@ export default function CoffeeCard({ imgsrc, name, roastLevel, price }: propType
 
         {/* Right column: Button */}
         <div className="flex justify-end">
-          <button className="bg-blue-500 text-white px-4 py-2 rounded-full hover:bg-amber-700 text-3xl">
-            +
+          {!isAlreadyInCart?(
+          <button type="button" onClick={addToCart} className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-full text-sm p-2.5 text-center inline-flex items-center me-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+            <svg className="w-6 h-6 text-white dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+              <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 4h1.5L8 16m0 0h8m-8 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4Zm8 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4Zm.75-3H7.5M11 7H6.312M17 4v6m-3-3h6"/>
+            </svg>
+            <span className="sr-only">Plus</span>
+          </button>)
+          :
+          (<button type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-full text-sm p-2.5 text-center inline-flex items-center me-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+            In Cart
+            <span className="sr-only">Plus</span>
           </button>
+          )}
         </div>
       </div>
     </div>
