b98f7774f205f0e99e85f18852c4e263b11a0eca
avneetvirdi <avneetvirdi26@gmail.com>
2025-07-16 08:13:59 -0400

    commit

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -1,9 +1,31 @@
 import Header from "@/component/Header";
+import { useCart } from "@/context/cartContext";
+
+type cartItemType = {
+  imgsrc: string;
+  name: string;
+  roastLevel: number;
+  price: number;
+};
 
 export default function Cart() {
+
+    const { cart, setCart } = useCart();
+
     return (
         <div>
             <Header showLinks={true} noBackground={false}/>
+
+            <section className="cart-container">
+                <h1>Your Cart</h1>
+                <div className="cart-container">
+                    {
+                        cart.map((cartItem:cartItemType) => (
+                            <li key={cartItem.name}>{cartItem.name}</li>
+                        ))
+                    }
+                </div>
+            </section>
         </div>
     )
 }
